d6f83a0828813964006fd61a71fed3b133eb9362
Poylar <doalesk8@gmail.com>
2023-03-10 17:26:02 +0800

    init

---
+++ src/assets/js/Modal.js
@@ -0,0 +1,40 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-alert */
+class Modal {
+  constructor(trigger, modal) {
+    this.modal = modal;
+    this.trigger = trigger;
+    this.closeBtn = '.js-modal-close';
+    this.state = false;
+  }
+
+  open() {
+    document.querySelector(this.modal).style.display = 'block';
+    this.state = true;
+  }
+
+  close() {
+    document.querySelector(this.modal).style.display = 'none';
+    this.state = false;
+  }
+
+  listener() {
+    document.addEventListener('click', (e) => {
+      if ((this.state && !e.target.closest('.modal')) || (this.state && e.target.closest(this.closeBtn))) {
+        this.close();
+      }
+      if (!this.state && e.target.closest(this.trigger)) {
+        this.open();
+      }
+    });
+  }
+}
+
+const modal = document.querySelectorAll('[data-modal]');
+
+modal.forEach((elem) => {
+  const val = elem.dataset.modal;
+
+  const modalInstance = new Modal(`[data-modal="${val}"]`, `[data-modal-el="${val}"]`);
+  modalInstance.listener();
+});
